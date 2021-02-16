1d332d283941b61d604b30041f295662e1e32d6c
norahmad95 <artistnor95@gmail.com>
2021-02-17 01:55:13 +0200

    cookie-stand

--- js/app.js
+++ js/app.js
@@ -2,49 +2,79 @@
 
 
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+const footerTotal = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function Location(name, min, max, avg, avgPerCustSales, salesPerHour, totalSeals, totalOfTotal) {
+function Location(name, min, max, avgPerCustSales, salesPerHour) {
 
     this.name = name,
         this.min = min,
         this.max = max,
-        this.avg = avg,
+        this.avg = [],
         this.avgPerCustSales = avgPerCustSales,
-        this.salesPerHour = salesPerHour,
-        this.totalSeals = totalSeals,
-        this.totalOfTotal = totalOfTotal
+        this.salesPerHour = [],
+        this.totalSeals = 0
 
 };
 
 Location.prototype.calc = function() {
     for (let i = 0; i < 14; i++) {
 
-        this.avg.push(getRndInteger(min, max));
+        this.avg.push(getRndInteger(this.min, this.max));
     }
 
     for (let j = 0; j < 14; j++) {
         salesPerHour.push(avgPerCustSales * avg[j]);
-        totalSeals += salesPerHour[j];
-
-        totalOfTotal = totalOfTotal + salesPerHour[j];
+        totalSeals += Math.ceil(this.salesPerHour);
+        footerTotal[j] += this.salesPerHour
+        this.salesPerHour.push(hours[n](perHourCookies));
 
     }
 };
 
-totalCookies = totalCookies + salesPerHour;
 
-const dataEl = document.createElement('td');
-tableRow.appendChild(dataEl);
-dataEl.textContent = Math.ceil(salesPerHour); {
-    footerTotals[14] += Math.ceil(totalCookies);
-    const dataEl = document.createElement('td');
-    tableRow.appendChild(dataEl);
-    dataEl.textContent = Math.ceil(totalCookies);
+Location.prototype.render = function() {
+
+    const continar = document.getElementById('seal');
+    const tableRow = document.createElement('tr');
+    continar.appendChild(tableRow);
+    // heading
+    const tableHeadingEl = document.createElement('th');
+    continar.appendChild(tableHeadingEl);
+    tableHeadingEl.textContent = this.name;
+
+
+    const footerTotal = 0
+    for (let n = 0; n <= 14; n++) {
+        getRndInteger(this.min, this.max);
+        let avgPerCustSales = this.getRndInteger * this.avgPerCustSales;
+        this.salesPerHour.push(`${hours[i]}: ${perHourCookies}`);
+        footerTotal[n] += Math.ceil(this.salesPerHour);
+        // this.salesPerHour.push(hours[n](perHourCookies));
+    }
+
+    //  creat data into the table
+    const totalCookies = 0
+    totalCookies = totalCookies + perHourCookies;
+    const saleDat = document.createElement('td');
+    tableRow.appendChild(saleDat);
+    dataEl.textContent = (perHourCookies);
+
+
+
+    footerTotal[14] += (totalCookies);
+    // const saleDat = document.createElement('td');
+    tableRow.appendChild(saleDat);
+    saleDat.textContent = (totalCookies);
 };
+
+
+
+
+
 
 
 // header for table 
@@ -68,33 +98,42 @@ function renderHeader() {
     lastTableHeadingEl.textContent = 'Daily Location Total';
 }
 
-// footer total of total 
+function firstRowTable(name, min, max, avgPerCustSales, salesPerHour, totalSeals, totalOfTotal) {
 
-function renderFooterTotals() {
-    const container = document.getElementById('sea');
+    const dataSales = document.createElement('seal');
+    dataRowEl.appendChild(dataCellEl);
+    dataCellEl.textContent = this.salesPerHour[i];
+    totalOfTotal[i] += this.salesPerHour[i];
+
+}
+
+// footer total of total
+
+function foterTotalOf() {
+    const continar = document.getElementById('seal');
     const tableRow = document.createElement('tr');
-    container.appendChild(tableRow);
+    continar.appendChild(tableRow);
 
     const tableHeadingEl = document.createElement('th');
     tableRow.appendChild(tableHeadingEl);
-    tableHeadingEl.setAttribute('scope', 'row');
-    tableHeadingEl.textContent = 'Totals';
-
-    for (let i = 0; i < footerTotals.length; i++) {
-        const tableHeadingEl = document.createElement('th');
-        tableRow.appendChild(tableHeadingEl);
-        tableHeadingEl.textContent = (footerTotals[i]);
-    }
-};
+    tableHeadingEl.textContent = (footerTotal[i])
 
 
 
+}
 
-const FiLocation = new Seattel('Seattel', 23, 65, [], 6.3, [], 0);
-const SeLocation = new Tokyo('Tokyo', 3, 24, [], 1.2, [], 0);
-const ThLocation = new Dubai('Dubai', 11, 38, [], 3.7, [], 0);
-const FoLocation = new Paris('Paris', 20, 38, [], 2.3, [], 0);
-const FeLocation = new Lima('Lima', 2, 16, [], 4.6, [], 0);
+function footerRow() {
+    const footRowEl = document.createElement('tr');
+    tableEl.appendChild(footRowEl);
+}
+
+
+
+const seattle = new Location('seattle', 23, 65, [], 6.3, [], 0);
+const tokyo = new Location('Tokyo', 3, 24, [], 1.2, [], 0, []);
+const dubai = new Location('Dubai', 11, 38, [], 3.7, [], 0, []);
+const paris = new Location('Paris', 20, 38, [], 2.3, [], 0, []);
+const lima = new Location('Lima', 2, 16, [], 4.6, [], 0, []);
 
 // function call
 
@@ -104,7 +143,7 @@ tokyo.render();
 dubai.render();
 paris.render();
 lima.render();
-renderFooterTotals();
+foterTotalOf();
 
 // lab 06
 // const seattle = {
@@ -466,5 +505,6 @@ renderFooterTotals();
 //             }
 
 //             lima.calc() console.log(lima.avg) lima.render() ima.avg)
+//         lima.render()() console.log(lima.avg) lima.render() ima.avg)
 //         lima.render()() console.log(lima.avg) lima.render() ima.avg)
 //         lima.render()
